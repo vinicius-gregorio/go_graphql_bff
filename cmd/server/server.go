50c6de16df4f9db57fc25f4efc120473c10edc3a
Vinicius Gregorio <vincamgreg@hotmail.com>
2023-09-05 22:18:58 -0300

    refact: move server.go to proper location

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/vinicius-gregorio/go_graphql_bff/graph"
+	"github.com/vinicius-gregorio/go_graphql_bff/internal/database"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("Err to open db: %v", err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
