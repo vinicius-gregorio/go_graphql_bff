48c2444dc7d6f241c36cdb94f893234d36086410
Vinicius Gregorio <vincamgreg@hotmail.com>
2023-09-06 21:27:38 -0300

    feat: add get all categories

--- internal/database/category.go
+++ internal/database/category.go
@@ -25,3 +25,24 @@ func (c *Category) Create(name string, description string) (Category, error) {
 	}
 	return Category{db: c.db, ID: id, Name: name, Description: description}, nil
 }
+
+func (c *Category) FindAll() ([]Category, error) {
+	rows, err := c.db.Query("SELECT id, name, description FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var categories []Category
+	for rows.Next() {
+		var id string
+		var name string
+		var description string
+		err = rows.Scan(&id, &name, &description)
+		if err != nil {
+			return nil, err
+		}
+		categories = append(categories, Category{ID: id, Name: name, Description: description})
+	}
+	return categories, nil
+}
